4aae85c1fff8002da665563ffde580a564425848
julianPavlov <62881010+julicq@users.noreply.github.com>
2023-03-16 10:15:35 +0100

    Refactoring main.py

--- main.py
+++ main.py
@@ -8,84 +8,83 @@ import pytesseract
 from pdf2image import convert_from_path
 from PIL import Image
 
-isort.file("main.py")
 
-logger = getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
+class OCRTool:
+    def __init__(self):
+        self.logger = getLogger(__name__)
+        logging.basicConfig(level=logging.INFO)
+
+    def process_pdf(self, source, result, verbose):
+        pdf_file = source
+        if verbose:
+            self.logger.info('PDF file is in input')
+        pages = convert_from_path(pdf_file, 500)
+        image_counter = 1
+        for page in pages:
+            filename = "page_" + str(image_counter) + ".jpg"
+            page.save(filename, 'JPEG')
+            image_counter = image_counter + 1
+        if verbose:
+            self.logger.info('{} pages from PDF retrieved'.format(image_counter - 1))
+
+        file_limit = image_counter - 1
+        outfile = result
+        f = open(outfile, "a")
+        if verbose:
+            self.logger.info('Output file {} created'.format(result))
+        for i in range(1, file_limit + 1):
+            filename = "page_" + str(i) + ".jpg"
+            text = str((pytesseract.image_to_string(Image.open(filename))))
+            text = text.replace('-\n', '')
+            f.write(text)
+            if verbose:
+                self.logger.info('Output file written')
+        f.close()
+
+    def process_image(self, source, result, verbose):
+        img = cv2.imread(source)
+        if verbose:
+            self.logger.info('Image file is in input')
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        if verbose:
+            self.logger.info('Converting image to gray palette')
+        blur = cv2.medianBlur(gray, 1)
+        if verbose:
+            self.logger.info('Image blur applied')
+        tessdata_dir_config = '--oem 2 --psm 12 --tessdata-dir "/usr/local/share/tessdata/"'
+        outfile = result
+        f = open(outfile, "a")
+        if verbose:
+            self.logger.info('Output file {} created'.format(result))
+        text = str(pytesseract.image_to_string(blur, lang='eng', config=tessdata_dir_config))
+        f.write(text)
+        if verbose:
+            self.logger.info('Output file filled with OCRd text')
+        f.close()
+
+    def run(self, source, result, verbose):
+        isort.file(source)
+        if source.split(".")[-1] == "pdf":
+            self.process_pdf(source, result, verbose)
+        elif source.split(".")[-1] == "png" or source.split(".")[-1] == "jpg":
+            self.process_image(source, result, verbose)
+        else:
+            self.logger.error('Unsupported file format')
 
 
 @click.command()
 @click.option('--input', '-i', required=True,
               help='Path to file(.pdf/.png/.jpg) to be processed',
-              type=click.Path(exists=True, dir_okay=False, readable=True), )
+              type=click.Path(exists=True, dir_okay=False, readable=True))
 @click.option('--output', '-o', default="output.text",
               help='Path to file to store result',
-              type=click.Path(dir_okay=False), )
+              type=click.Path(dir_okay=False))
 @click.option('--verbose', '-v', is_flag=True,
-              help='Verbose log mode', )
-def ocr(input, output, verbose):
-    source = input
-    result = output
-    # for Windows provide if getting error
-    # Example config: r'--tessdata-dir "C:\Program Files (x86)\Tesseract-OCR\tessdata"'
-    # It's important to add double quotes around the dir path.
-    tessdata_dir_config = '--oem 2 --psm 12 --tessdata-dir "/usr/local/share/tessdata/"'
-
-    while True:
-        if source.split(".")[-1] == "pdf":
-            pdf_file = source
-            if verbose:
-                logging.info('PDF file is in input')
-            pages = convert_from_path(pdf_file, 500)
-            image_counter = 1
-            for page in pages:
-                filename = "page_" + str(image_counter) + ".jpg"
-                page.save(filename, 'JPEG')
-                image_counter = image_counter + 1
-            if verbose:
-                logging.info('{} pages from PDF retrieved'.format(image_counter - 1))
-
-            file_limit = image_counter - 1
-            outfile = result
-            f = open(outfile, "a")
-            if verbose:
-                logging.info('Output file {} created'.format(result))
-            for i in range(1, file_limit + 1):
-                filename = "page_" + str(i) + ".jpg"
-                text = str((pytesseract.image_to_string(Image.open(filename))))
-                text = text.replace('-\n', '')
-                f.write(text)
-                if verbose:
-                    logging.info('Output file written')
-            f.close()
-            break
-        else:
-            if source.split(".")[-1] == "png" or source.split(".")[-1] == "jpg":
-                img = cv2.imread(source)
-                if verbose:
-                    logging.info('Image file is in input')
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-                if verbose:
-                    logging.info('Converting image to gray palette')
-                blur = cv2.medianBlur(gray, 1)
-                if verbose:
-                    logging.info('Image blur applied')
-                # thresh = cv2.threshold(blur, 10, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-                # if verbose:
-                #     logging.info('Image threshold applied')
-                outfile = result
-                f = open(outfile, "a")
-                if verbose:
-                    logging.info('Output file {} created'.format(result))
-                text = str(pytesseract.image_to_string(blur, lang='eng', config=tessdata_dir_config))
-                f.write(text)
-                if verbose:
-                    logging.info('Output file filled with OCRd text')
-                f.close()
-                break
-        print('Unsupported file format')
-        break
+              help='Verbose log mode')
+def main(input, output, verbose):
+    ocr_tool = OCRTool()
+    ocr_tool.run(input, output, verbose)
 
 
 if __name__ == "__main__":
-    ocr()
+    main()
